86e485dc7c0ca4f9407147e6a4e265b3b6458f56
Michal Zabinski <zabinski@student.agh.edu.pl>
2021-08-09 11:23:40 +0200

    Dodanie_funkcjonalnosci_czytania_z_pliku

--- Main.java
+++ Main.java
@@ -1,16 +1,34 @@
 package pl.zabinski;
 
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	String napis = "Lubie pizze hawajska";
-	napis = napis.replace(" ", "");
-	char[] tablica = new char[napis.length()];
+	public static void main(String[] args) {
+		File file = new File("plik.txt");
 
-	for (int i = 0;i < napis.length(); i++){
-	    tablica[i] = napis.charAt(i);
-    }
+		String napis = "";
+		try {
+			file.createNewFile();
+			Scanner fileScanner = new Scanner(file);
+			while (fileScanner.hasNext()) {
+				napis = napis + fileScanner.nextLine();
+			}
+			napis = napis.replace(" ", "");
+			char[] tablica = new char[napis.length()];
+			for (int i = 0; i < napis.length(); i++) {
+				tablica[i] = napis.charAt(i);
+			}
+			System.out.println(napis);
+			System.out.println("Dlugosc napisuj wynosci: " + tablica.length);
 
-	System.out.println("Dlugosc napisuj wynosci: " + tablica.length);
-    }
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+
+	}
 }
